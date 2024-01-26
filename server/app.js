f1e3b7b2c5f34c34e55b06d3d3c39696b3c4b4e1
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2024-01-25 23:58:16 -0500

    commit

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+//allow cross-origin requests
+app.use(cors());
 
 mongoose.connect('mongodb+srv://Vissu:6262@book-author.wocoemb.mongodb.net/?retryWrites=true&w=majority')
 mongoose.connection.once('open', () => {
